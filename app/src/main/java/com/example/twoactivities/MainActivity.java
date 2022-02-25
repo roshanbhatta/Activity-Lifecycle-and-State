9f691cbe9278293ed4277a1a7abb59f06feca945
Roshan Bhatta <roshanbha4tta4@gmail.com>
2022-02-25 08:28:14 +0545

    Two Activities Task 1-4 Completed

--- app/src/main/java/com/example/twoactivities/MainActivity.java
+++ app/src/main/java/com/example/twoactivities/MainActivity.java
@@ -23,16 +23,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mMessageEditText = findViewById(R.id.editText_main);
+        mReplyHeadTextView=findViewById((R.id.text_header_reply));
+        mReplyTextView=findViewById(R.id.text_message_reply);
     }
 
     public void launchSecondActivity(View view) {
-        Log.d(LOG_TAG,"Button Clicked!");
+        Log.d(LOG_TAG,"Button clicked!");
         Intent intent = new Intent(this, SecondActivity.class);
         String message = mMessageEditText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+        startActivityForResult(intent,TEXT_REQUEST);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
 
+        if(requestCode==TEXT_REQUEST){
+            if(resultCode==RESULT_OK){
+                String reply =
+                        data.getStringExtra(SecondActivity.EXTRA_REPLY);
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(reply);
+                mReplyTextView.setVisibility(View.VISIBLE);
 
+            }
+        }
     }
 
 }
--- app/src/main/java/com/example/twoactivities/SecondActivity.java
+++ app/src/main/java/com/example/twoactivities/SecondActivity.java
@@ -17,13 +17,11 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        mReply = findViewById((R.id.editText_second));
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         TextView textView = findViewById(R.id.text_message);
         textView.setText(message);
-        mReply = findViewById(R.id.editText_second);
-
-
     }
 
     public void returnReply(View view) {
