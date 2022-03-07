cbb3830beca1a4a666007a47308c72101dacc1fc
Roshan Bhatta <roshanbha4tta4@gmail.com>
2022-03-07 15:47:55 +0545

    Activities and Intents Homework

--- app/src/main/java/com/example/hellotoast/MainActivity.java
+++ app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -2,13 +2,17 @@ package com.example.hellotoast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    public static final String EXTRA_MESSAGE =
+            "com.example.android.twoactivities.extra.MESSAGE";
     private int mCount = 0;
     private TextView mShowCount;
     @Override
@@ -19,15 +23,18 @@ public class MainActivity extends AppCompatActivity {
         //Log.d(TAG,String.valueOf(count));//debug using log
     }
 
-    public void showToast(View view) {
-        Toast toast = Toast.makeText(this,R.string.toast_message,Toast.LENGTH_SHORT);
-        toast.show();
-    }
-
     public void countUp(View view) {
         mCount++;
         if(mShowCount != null){
             mShowCount.setText(Integer.toString(mCount));
         }
     }
+    public void launchSecondActivity(View view) {
+
+        String number = mShowCount.getText().toString();
+        Intent intent = new Intent(this,SecondActivity.class);
+        intent.putExtra(EXTRA_MESSAGE,number);
+        startActivity(intent);
+    }
+
 }
