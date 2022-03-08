7994bed0e171f1ae867b7000b665d99dabbec11f
Roshan Bhatta <roshanbha4tta4@gmail.com>
2022-03-08 15:07:45 +0545

    Shopping List Challange Completed

--- app/src/main/java/com/example/shoppinglistchallange/MainActivity.java
+++ app/src/main/java/com/example/shoppinglistchallange/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.shoppinglistchallange;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int TEXT_REQUEST = 1;
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void launchSecondActivity(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String itemName =  data.getStringExtra(SecondActivity.EXTRA_REPLY);
+                for (int index = 1; index <=10; index++) {
+                    String textViewName = "item_list" + index;
+                    int id =getResources().getIdentifier(textViewName,"id",getPackageName());
+                    TextView textView = (TextView) findViewById(id);
+                    if (textView.getText() == "") {
+                        textView.setText(textView.getText() + itemName.toUpperCase(Locale.ROOT));
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/shoppinglistchallange/SecondActivity.java
+++ app/src/main/java/com/example/shoppinglistchallange/SecondActivity.java
@@ -2,13 +2,28 @@ package com.example.shoppinglistchallange;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
+    private static final String LOG_TAG = SecondActivity.class.getSimpleName();
+    public static final String EXTRA_REPLY =
+            "com.example.shoppinglistchallange.extra.REPLY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+    }
+    public void addItem(View view){
+        String itemName = ((Button)view).getText().toString();
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_REPLY, itemName);
+        setResult(RESULT_OK, intent);
+        Log.d(LOG_TAG, "End SecondActivity");
+        finish();
     }
 }
